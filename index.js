c1452022709b0b6c292bf2c3de665c441c997cd2
chinbaa <janchivchinbaa@gmail.com>
2024-12-02 17:17:35 +0800

    const

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+
+
+const firstName="Bold"
+console.log(firstName)
+const lastName="chinbaa"
+console.log(lastName)
+const materialStatus ="mater"
+console.log(materialStatus)
+const country="ulaanbaatar"
+console.log(country)
+let age = "13"
+console.log(age, age, age, age)
